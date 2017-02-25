3ee4040127d92435996d748b3580d63061020566
hsz <jakub@chrzanowski.info>
2017-02-25 10:23:51 +0100

    updated text, added plugin url to JB site

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ import request from 'request-promise';
 import sortBy from 'lodash/sortBy';
 
 const PLUGIN_URL = 'https://plugins.jetbrains.com/idea/plugin';
+const DATE = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
 
 console.log('# Jetbrains Plugins Repositories List');
 console.log('## This is a listing of all JetBrains (IntelliJ, Android Studio, PhpStorm, WebStorm, ' +
     'PyCharm, AppCode) 3rd party plugins repositories.');
-console.log('I don\'t count here all plugins (because they can be found [here](https://plugins.jetbrains.com/) ' +
-    '- only those with specified URL to the public sources repository (BitBucket or GitHub).\n');
+console.log('Not every plugin is counted here- only those with specified URL to the public sources repository ' +
+    '(BitBucket or GitHub). Full list of the plugins is available [here](https://plugins.jetbrains.com/).\n');
 console.log('This repository can be used as a good starting point for plugins development - it\'s always good ' +
     'to check how the others have solved their problems!\n');
 console.log('*Good luck!*\n\n');
+console.log(`Generated: _${DATE}_\n\n\n`);
 console.log('| Plugin name | Downloads | Last update | Repository |');
 console.log('| ----------- |:---------:|:-----------:| ---------- |');
 
@@ -20,7 +22,7 @@ const fetch = (max, parse, deferred = Q.defer(), promise = deferred.promise) => 
   for (let i = 1; i <= max; i++) {
     promise = promise.then((results) =>
         request(`${PLUGIN_URL}/${i}`)
-            .then(result => results.concat(parse(cheerio.load(result))))
+            .then(result => results.concat(parse(i, cheerio.load(result))))
             .catch(() => results)
     );
   }
@@ -28,10 +30,11 @@ const fetch = (max, parse, deferred = Q.defer(), promise = deferred.promise) => 
   return promise;
 };
 
-fetch(10000, ($) => ({
+fetch(50, (i, $) => ({
   name: $('.plugin-title').html().split('<br>').shift().trim(),
   update: $('.plugin-info__update').text().trim(),
   downloads: $('.plugin-info__downloads').text().trim(),
+  pluginUrl: `${PLUGIN_URL}/${i}`,
   url: $('.sidebar-box a')
       .map((i, el) => $(el).attr('href'))
       .toArray()
@@ -39,6 +42,6 @@ fetch(10000, ($) => ({
       .shift(),
 }))
     .then(response => sortBy(response.filter(({url}) => url), 'name'))
-    .then(response => response.map(({name, downloads, update, url}) =>
-        console.log(`| ${name} | ${downloads} | ${update} | ${url} |`))
+    .then(response => response.map(({name, downloads, update, pluginUrl, url}) =>
+        console.log(`| [${name}[(${pluginUrl}) | ${downloads} | ${update} | ${url} |`))
     );
