d418e8c71bd538923837889ecbcb5d5b630d5cd4
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2020-03-08 15:57:18 +0100

    dependencies upgrade, integrate with analyzer, configuration cleanup

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,24 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { addBabelPlugin, override, addLessLoader } = require('customize-cra');
 
 module.exports = override(
-  fixBabelImports('import', {
-    libraryName: 'antd',
-    libraryDirectory: 'es',
-    style: 'css',
+  addBabelPlugin(['babel-plugin-emotion', { sourceMap: true, autoLabel: true }]),
+  addBabelPlugin([
+    'import',
+    {
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: true,
+    },
+  ]),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: {
+      // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+    },
+  }),
+  config => ({
+    ...config,
+    plugins: config.plugins.concat([...(process.env.ANALYZER ? [new BundleAnalyzerPlugin()] : [])]),
   }),
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,23 @@
+import { css, Global } from '@emotion/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const globalStyles = css`
+  body,
+  html {
+    height: 100%;
+  }
+`;
+
+ReactDOM.render(
+  <>
+    <Global styles={globalStyles} />
+    <App />
+  </>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
